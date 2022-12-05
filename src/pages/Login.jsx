586608eb172c2615a63ede530db506c74e6b08d5
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-12-06 00:19:52 +0530

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { Container } from "./Styled";
+import {
+  Button,
+  GButton,
+  Container,
+  Divider,
+  Heading,
+  Horiz,
+  Input,
+  Label,
+  LoginWrapper,
+} from "./Styled";
 
 function Login() {
   useEffect(() => {
@@ -10,7 +20,31 @@ function Login() {
   return (
     <React.Fragment>
       <Navbar />
-      <Container>Login</Container>
+      <Container>
+        <LoginWrapper>
+          <Heading>Log in</Heading>
+          <Label>Email address</Label>
+          <Input type="email" />
+          <Label>Password</Label>
+          <Input type="password" />
+
+          <Button bg="#20B2AA" color="white">
+            Log in
+          </Button>
+          <Button bg="transparent" color="#20B2AA">
+            Forgot your password?
+          </Button>
+          <Divider>
+            <Horiz />
+            or
+            <Horiz />
+          </Divider>
+
+          <GButton bg="white" color="#515151">
+            Sign in with Google
+          </GButton>
+        </LoginWrapper>
+      </Container>
       <Footer />
     </React.Fragment>
   );
--- src/pages/Styled.js
+++ src/pages/Styled.js
@@ -5,3 +5,84 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+
+//Styled-Components for Login
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  width: 30vw;
+
+  margin: 50px 0;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  @media (max-width: 768px) {
+    width: 70vw;
+  }
+`;
+
+export const Heading = styled.div`
+  font-size: 26px;
+  margin-bottom: 20px;
+`;
+
+export const Label = styled.div`
+  color: #515151;
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  padding: 10px;
+  background: transparent;
+  border-radius: 5px;
+  border: none;
+  border: 1px solid #515151;
+  margin-bottom: 10px;
+  font-size: 15px;
+  letter-spacing: 2px;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: ${(props) => props.bg};
+  color: ${(props) => props.color};
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  margin: 5px 0;
+  &:hover {
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  }
+`;
+export const GButton = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: ${(props) => props.bg};
+  color: ${(props) => props.color};
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  margin: 5px 0;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+`;
+
+export const Horiz = styled.div`
+  border-top: 1px solid #dfdfdf;
+  width: 70px;
+  margin: 5px;
+`;
+
+export const Divider = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  color: #dfdfdf;
+  font-weight: bold;
+`;
